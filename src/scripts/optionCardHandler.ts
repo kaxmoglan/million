b570fb01611df03e4d4c850b70944de8b270a0ba
Maximilian Coghlan <max.coghlan@gmail.com>
2023-08-13 21:41:52 +0100

    optionCardHandler refactor

--- src/scripts/optionCardHandler.ts
+++ src/scripts/optionCardHandler.ts
@@ -1,30 +1,16 @@
 import { OptionCardId, getFormSubject } from '../utils/optionCardUtils';
 
-// to check if url has changed
-let existingUrlSubject: string | null = null;
-let subject: string | null = null;
-
 const handleSubject = () => {
 	const url = window.location;
 	if (!url.href.includes('/contact')) return;
 
 	const urlParams = new URLSearchParams(url.search);
-	const newUrlSubject = urlParams.get('selected-option');
+	const selectedOption = urlParams.get('selected-option');
 
-	const hasUrlSubjectChanged = newUrlSubject !== existingUrlSubject;
-
-	if (!newUrlSubject || (!subject && !hasUrlSubjectChanged)) {
-		return;
-	}
-
-	if (hasUrlSubjectChanged || !subject) {
-		existingUrlSubject = newUrlSubject;
-		subject = getFormSubject(newUrlSubject as OptionCardId);
-	}
+	const subject = getFormSubject(selectedOption as OptionCardId);
 
 	const subjectText = document.getElementById('subject-text');
 	if (subjectText) subjectText.innerHTML = subject;
 };
 
-handleSubject();
-setInterval(handleSubject, 200);
+document.addEventListener('astro:beforeload', handleSubject);

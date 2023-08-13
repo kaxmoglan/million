da2d2b22f2c7dc40f74a30cac2ad2478bc9441ca
Maximilian Coghlan <max.coghlan@gmail.com>
2023-08-13 22:19:34 +0100

    host page + commonise page header styling

--- src/utils/optionCardUtils.ts
+++ src/utils/optionCardUtils.ts
@@ -5,6 +5,9 @@ export enum OptionCardId {
 	BUILD_WEBSITES = 'build-websites',
 	BUILD_WEB_APP = 'build-web-app',
 	BUILD_NATIVE_APP = 'build-native-app',
+	HOST_START_UP = 'host-start-up',
+	HOST_SCALE_UP = 'host-scale-up',
+	HOST_BESPOKE = 'host-bespoke',
 }
 
 export const getFormSubject = (buttonId: OptionCardId): string => {
@@ -26,6 +29,15 @@ export const getFormSubject = (buttonId: OptionCardId): string => {
 
 		case OptionCardId.BUILD_NATIVE_APP:
 			return `Native App Building Enquiry`;
+
+		case OptionCardId.HOST_START_UP:
+			return `Start Up Hosting Package`;
+
+		case OptionCardId.HOST_SCALE_UP:
+			return `Scale Up Hosting Package`;
+
+		case OptionCardId.HOST_BESPOKE:
+			return `Bespoke Hosting`;
 
 		default:
 			throw new Error('Unknown OptionCardId');
